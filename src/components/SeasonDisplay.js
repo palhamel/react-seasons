62ba28312f08e582b71b9ad94e945d3086689e29
Pål Hamel <pal.hamel@gmail.com>
2020-07-28 12:35:10 +0200

    Setup nav position callback

---
+++ src/components/SeasonDisplay.js
@@ -0,0 +1,10 @@
+//rafc
+import React from 'react'
+
+export const SeasonDisplay = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import SeasonDisplay from './components/SeasonDisplay'
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    // the success callback:
+    (position) => console.log('my position is:', position),
+    // failure callback:
+    (err) => console.log('Houston, we have an error:', err)
+  );
+
   return <div>react setup complete</div>;
 };
 
