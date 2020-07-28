b7aff0f9ae99703d8950a185a5e52a1d2ab7e79f
Pål Hamel <pal.hamel@gmail.com>
2020-07-28 16:24:09 +0200

    Set up contructor with init state

--- src/index.js
+++ src/index.js
@@ -2,15 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import SeasonDisplay from './components/SeasonDisplay'
 
+// rcjc - Class based component:
+
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  // req by React: 'render()'
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      // the success callback:
+      (position) => console.log("my position is:", position),
+      // failure callback:
+      (err) => console.log("Houston, we have an error:", err)
+    );
+    return <div>latitude:</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+// refactor component
+// functional component:
+/*
+
 const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     // the success callback:
-    (position) => console.log('my position is:', position),
+    (position) => console.log("my position is:", position),
     // failure callback:
-    (err) => console.log('Houston, we have an error:', err)
+    (err) => console.log("Houston, we have an error:", err)
   );
 
-  return <div>react setup complete</div>;
+  return <div>Lat:</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+*/
