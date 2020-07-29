f69724dbe4a29c9a5986ea553f80fa5db963a808
Pål Hamel <pal.hamel@gmail.com>
2020-07-29 20:57:57 +0200

    Add default props to spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const Spinner = () => {
+export const Spinner = (props) => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading..</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
-  )
-}
+  );
+};
+
+Spinner.defaultProps = {
+  message: "Loading..",
+};
--- src/index.js
+++ src/index.js
@@ -48,7 +48,8 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat}/>;
     }
 
-    return <Spinner />;
+    // return <Spinner />;
+    return <Spinner message="Please accept location request"/>;
   }
 }
 
