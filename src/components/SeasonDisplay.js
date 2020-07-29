92a9e35ef8c6b2bce5e856387a61754c554fb30b
Pål Hamel <pal.hamel@gmail.com>
2020-07-29 16:38:21 +0200

    Add ternary expressions for message

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,11 +1,31 @@
 //rafc
-import React from 'react'
+import React from "react";
+
+// Determine season
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 
 export const SeasonDisplay = (props) => {
-  console.log(props.lat)
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  const textMessage =
+    season === "winter"
+      ? "Wintertime - cold and fresh!"
+      : "Summertime - warm and sunny";
+  const iconMessage = season === 'winter' ? 'snowflake' : 'sun';
+
+
+
   return (
     <div>
-      {props.lat}
+      <i className={`${iconMessage} icon`}/>
+      <h1>{textMessage}</h1>
+      <i className={`${iconMessage} icon`}/>
     </div>
-  )
-}
+  );
+};
